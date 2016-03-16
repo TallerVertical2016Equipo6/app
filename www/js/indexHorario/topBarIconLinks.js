35e2dd849607870d2dd0a4667d737424c482cd23
GabrielAvilesR <gabrielaviles1995@gmail.com>
2016-03-15 23:19:42 -0600

    Adding Top bar

---
+++ www/js/indexHorario/topBarIconLinks.js
@@ -0,0 +1,10 @@
+//topBarIcon AreaMenu
+
+//links al mapa
+$(document).ready(function(){
+	//console.log("ready");
+	$("#backArrow").click(function(){
+		$(location).attr('href','index.html');
+	});
+	
+});
