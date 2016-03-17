025c54f64d74399d9a868c736961b60150b81613
GabrielAvilesR <gabrielaviles1995@gmail.com>
2016-03-16 22:09:43 -0600

    saving b4 pull

---
+++ js/AreaMenu/loadAreas.js
@@ -0,0 +1,18 @@
+//hacer arreglo y cargar todas las zonas
+var containerCars = document.getElementById("containerCars");
+var areas = ["A", "B"];
+
+function loadAreas(userType , area){
+	if(userType === "visitante"){
+		//desplegar solo las zonas que son para visitantes
+	}else{
+		if(area==="nulo"){
+			//desplegar todas las areas en orden indistinto
+		}else{
+			var chosenArea = area;
+			
+		}
+
+	}
+
+}
