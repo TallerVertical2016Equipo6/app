99cbba01a55f4e3828a5cfa3b5675239961de2dc
Kevin <kevintroko@gmail.com>
2016-03-17 17:06:52 -0600

    boton horario recibe valores

--- js/indexHorario/topBarIconLinks.js
+++ js/indexHorario/topBarIconLinks.js
@@ -8,12 +8,30 @@ $(document).ready(function(){
 	});
     
     // inicia el dropdown 
-    $('.ui.dropdown')
-        .dropdown()
-    ;	
+    //$('.ui.dropdown')
+      //  .dropdown()
+    //;	
+    
     
    /*$('#monHorario')
-    .dropdown('set value', "B")
+     .dropdown('set text', function(){
+        $.jStorage.get('monday')
+     })
     ;*/
-    console.log("hola");
+    
+    $('#monHorario')
+     .dropdown('set text','B')
+    ;
+    
+        
+    //Funcion para boton 
+    $('#done').on('click', function(){
+        console.log(
+            $('#monHorario')
+             .dropdown('get value', "#monHorario"
+            )
+        );
+        console.log("hola");   
+    })
+    
 });			
